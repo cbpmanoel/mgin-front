2ecfa58b58f058050487edc79fe351a83206f60f
Manoel Pereira <cbp.manoel@gmail.com>
2024-12-29 19:23:15 -0300

    Add SelectQuantityModal component for item quantity selection

---
+++ src/components/SelectQuantityModal.vue
@@ -0,0 +1,161 @@
+<template>
+    <!-- Page overlay to highlight modal. Cancel is called if click outside the modal area -->
+    <div @click="onCancel" class="z-40 modal-overlay" />
+    <!-- Modal container -->
+    <div class="z-50 modal-window">
+        <!-- Modal content -->
+        <div class="modal-content">
+            <img :src="item.image" alt="Product Image" />
+            <h2 class="text-lg font-semibold">
+                <p>
+                    Select
+                    <span style="font-style: italic">{{ item.name }}</span>
+                    's quantity
+                </p>
+            </h2>
+
+            <!-- Quantity control area -->
+            <div class="cell-center">
+                <CustomButton
+                    :color="'blue'"
+                    :class="'w-8 h-8'"
+                    :disabled="item.quantity <= item.minQuantity"
+                    @click="onDecreaseQuantity"
+                >
+                    <object v-html="minusIco" v-once />
+                </CustomButton>
+                <span class="w-8 h-8 cell-center">{{ item.quantity }}</span>
+                <CustomButton
+                    :color="'blue'"
+                    :class="'w-8 h-8'"
+                    :disabled="item.quantity >= item.maxQuantity"
+                    @click="onIncreaseQuantity"
+                >
+                    <object v-html="plusIco" v-once />
+                </CustomButton>
+            </div>
+            <!-- Cancel, Remove and Confirm buttons -->
+            <div class="flex justify-between w-full">
+                <CustomButton :color="'gray'" @click="onCancel">
+                    Cancel
+                </CustomButton>
+                <!-- Remove button only appears when the item already is on the cart -->
+                <CustomButton
+                    v-if="quantityOnOpen > 0"
+                    :color="'red'"
+                    @click="onRemove"
+                >
+                    Remove
+                </CustomButton>
+
+                <CustomButton :color="'blue'" @click="onConfirm">
+                    {{ confirmationButtonText }}
+                </CustomButton>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { ref, defineEmits, computed } from "vue";
+import CustomButton from "@/components/CustomButton.vue";
+import minusIco from "@/assets/minus.svg?raw";
+import plusIco from "@/assets/plus.svg?raw";
+
+const props = defineProps({
+    itemName: {
+        type: String,
+        required: true,
+    },
+    itemImage: {
+        type: String,
+        required: false,
+        default: "",
+    },
+    itemQuantity: {
+        type: Number,
+        required: true,
+        default: 0,
+    },
+    minQuantity: {
+        type: Number,
+        required: false,
+        default: 0,
+    },
+    maxQuantity: {
+        type: Number,
+        required: false,
+        default: 10,
+    },
+});
+
+const quantityOnOpen = ref(props.itemQuantity);
+
+// Initialize the item references
+const item = ref({
+    name: props.itemName,
+    quantity: props.itemQuantity,
+    image: props.itemImage,
+    minQuantity: props.minQuantity,
+    maxQuantity: props.maxQuantity,
+});
+
+// Compute the confirm's button text the explicitly show
+// that the quantity will be changed if the item is already
+// on the cart.
+const confirmationButtonText = computed(() => {
+    if (quantityOnOpen.value === 0) {
+        return "Add to cart";
+    }
+    return "Update quantity";
+});
+
+const emit = defineEmits(["confirm", "cancel"]);
+
+// Callbacks
+const onCancel = () => {
+    console.log("Modal cancelled");
+    emit("cancel");
+};
+
+// Pass quantity as 0 to remove the item
+const onRemove = () => {
+    console.log("Remove item");
+    emit("confirm", { quantity: 0 });
+};
+
+// Return the selected quantity
+const onConfirm = () => {
+    console.log("Modal confirmed");
+    emit("confirm", { quantity: item.value.quantity });
+};
+
+// Quantity area manipulators
+const onIncreaseQuantity = () => {
+    if (item.value.quantity < props.maxQuantity) {
+        item.value.quantity++;
+    }
+    console.log("onIncreaseQuantity:", item.value.quantity);
+};
+
+const onDecreaseQuantity = () => {
+    if (item.value.quantity > props.minQuantity) {
+        item.value.quantity--;
+    }
+    console.log("onDecreaseQuantity:", item.value.quantity);
+};
+</script>
+
+<style scoped>
+.modal-overlay {
+    @apply fixed inset-0 bg-black opacity-70;
+}
+
+.modal-window {
+    @apply fixed inset-0 w-[300px] h-[300px] bg-gray-100 shadow-lg top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-md p-2;
+}
+
+.modal-content {
+    @apply flex flex-col justify-center items-center;
+}
+</style>
--- src/views/ProductsView.vue
+++ src/views/ProductsView.vue
@@ -1,39 +1,53 @@
 <template>
-    <!-- Side panel: Categories and Checkout area -->
-    <div class="flex w-full p-4 screen-height">
-        <div class="flex-shrink-0 w-1/4 min-w-0">
-            <SidePanel
-                :categories="categories"
-                :totalItems="totalItems"
-                :totalPrice="totalPrice"
-                @scroll-to="scrollTo"
-                @navigate-to-cart="onNavigateToCart"
-            />
-        </div>
+    <div>
+        <transition name="modal">
+            <div :key="showModal">
+                <SelectQuantityModal
+                    v-if="showModal"
+                    :itemName="selectedItem.name"
+                    :itemImage="selectedItem.imageSrc"
+                    :itemQuantity="selectedItem.quantity"
+                    @cancel="showModal = false"
+                    @confirm="onModalConfirm"
+                />
+            </div>
+        </transition>
+        <!-- Side panel: Categories and Checkout area -->
+        <div class="flex w-full p-2 screen-height">
+            <div class="flex-shrink-0 w-1/4 min-w-0">
+                <SidePanel
+                    :categories="categories"
+                    :totalItems="totalItems"
+                    :totalPrice="totalPrice"
+                    @scroll-to="scrollTo"
+                    @navigate-to-cart="onNavigateToCart"
+                />
+            </div>
 
-        <!-- Main content: Products separated by category -->
-        <!-- Category ref is used to scroll to the category section -->
-        <div class="w-3/4 ml-4 overflow-y-auto bg-gray-100 shadow-xl">
-            <div
-                v-for="category in productList"
-                :key="category.category"
-                class="bg-gray-100 main-content"
-                :ref="setCategoryRef(category.category)"
-            >
-                <div v-if="category.items.length > 0" class="mb-10">
-                    <!-- Item cards -->
-                    <GridContainer :title="category.category">
-                        <ItemCard
-                            v-for="item in category.items"
-                            :key="item.id"
-                            :id="item.id"
-                            :categoryId="item.categoryId"
-                            :description="item.name"
-                            :price="item.price"
-                            :imageSrc="item.imageSrc"
-                            @add-to-cart="onAddToCart"
-                        />
-                    </GridContainer>
+            <!-- Main content: Products separated by category -->
+            <!-- Category ref is used to scroll to the category section -->
+            <div class="w-3/4 ml-4 overflow-y-auto bg-gray-100 shadow-xl">
+                <div
+                    v-for="category in productList"
+                    :key="category.category"
+                    class="bg-gray-100 main-content"
+                    :ref="setCategoryRef(category.category)"
+                >
+                    <div v-if="category.items.length > 0" class="mb-10">
+                        <!-- Item cards -->
+                        <GridContainer :title="category.category">
+                            <ItemCard
+                                v-for="item in category.items"
+                                :key="item.id"
+                                :id="item.id"
+                                :categoryId="item.categoryId"
+                                :description="item.name"
+                                :price="item.price"
+                                :imageSrc="item.imageSrc"
+                                @add-to-cart="onAddToCart"
+                            />
+                        </GridContainer>
+                    </div>
                 </div>
             </div>
         </div>
@@ -49,6 +63,15 @@ import { useCart } from "@/composables/useCart";
 import { useRouterNavigation } from "@/composables/useRouterNavigation";
 import { useCategories } from "@/composables/useCategories";
 import { useProducts } from "@/composables/useProducts";
+import SelectQuantityModal from "@/components/SelectQuantityModal.vue";
+
+// Modal controllers
+const showModal = ref(true);
+const selectedItem = ref({
+    name: "Product name",
+    imageSrc: "https://placehold.co/200x200",
+    quantity: 4,
+});
 
 // Cart composable
 const { totalCartValue, totalItemsInCart, updateProductQty /* addToCart */ } =
@@ -91,6 +114,22 @@ const productList = computed(() => {
 });
 
 // Callbacks
+function onModalConfirm(payload) {
+    // Update the item's quantity
+    updateProductQty(
+        {
+            id: payload.id,
+            categoryId: payload.categoryId,
+            price: payload.price,
+            name: payload.name,
+        },
+        payload.quantity,
+    );
+
+    // Hide the modal
+    showModal.value = false;
+}
+
 function onAddToCart(payload) {
     console.log("Add to cart");
     console.log(payload);
@@ -128,5 +167,16 @@ const scrollTo = (category) => {
 <style scoped>
 .main-content {
     @apply justify-center items-center p-4;
+}
+
+/* Smooth fade + slight scale effect */
+.modal-enter-active,
+.modal-leave-active {
+    transition: all 0.3s ease;
+}
+.modal-enter-from,
+.modal-leave-to {
+    opacity: 0;
+    transform: scale(0.95);
 }
 </style>
