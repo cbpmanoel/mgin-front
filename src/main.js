bed6cd9604bf27bfd6cbe2ec56476c7e4b60c732
Manoel Pereira <cbp.manoel@gmail.com>
2025-01-02 18:00:56 -0300

    Remove Pinia integration from main application setup

--- src/main.js
+++ src/main.js
@@ -3,15 +3,9 @@ import "./assets/css/main.css";
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
-import { createPinia } from "pinia";
-import piniaPluginPersistedState from "pinia-plugin-persistedstate";
-
-const pinia = createPinia();
-pinia.use(piniaPluginPersistedState);
 
 const app = createApp(App);
 
 app.use(router);
-app.use(pinia);
 
 app.mount("#app");
