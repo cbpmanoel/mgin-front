ad321c754c8893d503e9c9eed9b130db2bdfb47e
Manoel Pereira <cbp.manoel@gmail.com>
2025-01-02 14:05:38 -0300

    Refactor cart logic to use Map

--- src/composables/useCart.js
+++ src/composables/useCart.js
@@ -1,94 +1,123 @@
 import { ref, computed } from "vue";
 
-const cart = ref([]);
+// Cart Object
+const cart = ref(new Map());
+
+// Validate a product object
+function validateProduct(product) {
+    if (!product.id || !product.categoryId) {
+        throw new Error("Product must have an id and a categoryId");
+    }
+    if (!product.price || product.price <= 0) {
+        throw new Error("Product must have a price greater than 0");
+    }
+    if (!product.name) {
+        throw new Error("Product must have a name");
+    }
+}
+
+// Returns a unique identifier for a product based on its id and categoryId
+function getProductUniqueId(product) {
+    return `${product.categoryId}-${product.id}`;
+}
+
+// Find a product in the cart
+function findProduct(product) {
+    return cart.value.get(getProductUniqueId(product));
+}
+
+// Create product object
+function createProduct(product, quantity = 1) {
+    return {
+        ...product,
+        quantity: quantity,
+    };
+}
 
 export const useCart = () => {
     // Add a product to the cart
     const addToCart = (product) => {
-        const productIndex = findProductIndex(product);
-
-        // If the product is not in the cart, add it
-        if (productIndex === -1) {
-            cart.value.push({ ...product, quantity: 1 });
-        }
-
-        // Otherwise, increase the quantity of the product in the cart
-        else {
-            cart.value[productIndex].quantity += 1;
-        }
+        updateProductQty(product, 1);
     };
 
     // Check if the cart contains a product
     const containsProduct = (product) => {
-        return findProductIndex(product) !== -1;
+        return findProduct(product) !== undefined;
     };
 
     // Remove the product from the cart
     const removeFromCart = (product) => {
-        const productIndex = findProductIndex(product);
-        if (productIndex !== -1) {
-            cart.value.splice(productIndex, 1);
-        }
+        updateProductQty(product, 0);
     };
 
     // Increase the quantity of a product in the cart
-    const increaseProductQty = (product) => {
-        const productIndex = findProductIndex(product);
-        if (productIndex !== -1) {
-            cart.value[productIndex].quantity += 1;
+    const increaseProductQty = (product, amount = 1) => {
+        if (amount <= 0) {
+            throw new Error("Amount must be greater than 0");
+        }
+
+        const existingProduct = findProduct(product);
+        if (existingProduct) {
+            updateProductQty(product, existingProduct.quantity + amount);
         }
     };
 
     // Decrease the quantity of a product in the cart
-    const decreaseProductQty = (product) => {
-        // Decrease the quantity of the product in the cart
-        const productIndex = findProductIndex(product);
-        if (productIndex !== -1) {
-            console.log(cart.value[productIndex]);
-            if (cart.value[productIndex].quantity > 1) {
-                cart.value[productIndex].quantity -= 1;
-            }
-            // Remove the product from the cart if its the last one
-            else {
-                removeFromCart(product);
-            }
+    const decreaseProductQty = (product, amount = 1) => {
+        if (amount <= 0) {
+            throw new Error("Amount must be greater than 0");
+        }
+
+        const existingProduct = findProduct(product);
+        if (existingProduct) {
+            updateProductQty(product, existingProduct.quantity - amount);
         }
     };
 
     // Update the quantity of a product in the cart
-    const updateProductQty = (productId, quantity) => {
-        const productIndex = findProductIndex(productId);
-        if (productIndex !== -1) {
-            if (quantity <= 0) {
-                cart.value.splice(productIndex, 1);
-            } else {
-                cart.value[productIndex].quantity = quantity;
-            }
+    const updateProductQty = (product, quantity) => {
+        validateProduct(product);
+        const existingProduct = findProduct(product);
+
+        // If the quantity is 0 or negative, remove the product from the cart
+        if (quantity <= 0) {
+            cart.value.delete(getProductUniqueId(product));
         }
-        // If the product is not in the cart, add it and set the quantity
-        else if (quantity > 0) {
-            addToCart(productId);
-            updateProductQty(productId, quantity);
+        // If the product is not in the cart, add it
+        else if (!existingProduct) {
+            cart.value.set(
+                getProductUniqueId(product),
+                createProduct(product, quantity),
+            );
+        }
+        // Otherwise, update the quantity of the product in the cart
+        else {
+            cart.value.set(getProductUniqueId(product), {
+                ...existingProduct,
+                quantity,
+            });
         }
     };
 
     // Clear the cart
     const clearCart = () => {
-        cart.value = [];
+        cart.value.clear();
     };
 
     // Compute the total value of the cart
     const totalCartValue = computed(() => {
-        console.log(cart.value);
-        return cart.value.reduce(
-            (acc, product) => acc + product.price * product.quantity,
+        return Array.from(cart.value.values()).reduce(
+            (total, product) => total + product.price * product.quantity,
             0,
         );
     });
 
     // Compute the total number of items in the cart
     const totalItemsInCart = computed(() => {
-        return cart.value.reduce((acc, product) => acc + product.quantity, 0);
+        return Array.from(cart.value.values()).reduce(
+            (total, product) => total + product.quantity,
+            0,
+        );
     });
 
     return {
@@ -109,11 +138,3 @@ export const useCart = () => {
         totalItemsInCart,
     };
 };
-
-// Auxiliary function to find the index of a product in the cart
-// Returns -1 if the product is not in the cart, otherwise the index
-function findProductIndex(product) {
-    return cart.value.findIndex(
-        (p) => p.id === product.id && p.categoryId === product.categoryId,
-    );
-}
