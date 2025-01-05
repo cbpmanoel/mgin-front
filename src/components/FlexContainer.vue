5838fe8c299b8eb953f7f670ac21709253b66159
Manoel Pereira <cbp.manoel@gmail.com>
2025-01-05 16:23:42 -0300

    Replace GridContainer with FlexContainer component for improved layout flexibility

---
+++ src/components/FlexContainer.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="title">
+        <slot name="header" />
+    </div>
+    <section class="grid-container">
+        <slot name="body" />
+    </section>
+</template>
+
+<style scoped>
+.grid-container {
+    @apply flex gap-4 flex-wrap justify-center sm:justify-start;
+}
+
+.title {
+    @apply text-2xl font-bold mb-2;
+}
+</style>
