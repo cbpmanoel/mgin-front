449dc41d6ac83e32908f21ebd870ed8276551ef9
Manoel Pereira <cbp.manoel@gmail.com>
2025-01-05 16:59:00 -0300

    Refactor HeaderBar component to improve button visibility and responsiveness based on screen size

--- src/components/HeaderBar.vue
+++ src/components/HeaderBar.vue
@@ -1,17 +1,25 @@
 <template>
     <div class="headerbar">
         <div class="headerbar-content">
-            <button class="headerbar-left" @click="navigateToProducts">
+            <button
+                v-if="showLeftButton"
+                class="headerbar-btn-left"
+                @click="navigateToProducts"
+            >
                 <object v-html="categoryIcon" v-once />
-                <div class="headerbar-text">Go To Products</div>
+                <div class="headerbar-text">
+                    {{ isSmallScreen ? "<" : "Products" }}
+                </div>
             </button>
             <div class="headerbar-center">
                 <div class="headerbar-title">
                     {{ currentRoute }}
                 </div>
             </div>
-            <button class="headerbar-right" @click="navigateToCart">
-                <div class="headerbar-text">Go To Cart</div>
+            <button class="headerbar-btn-right" @click="navigateToCart">
+                <div class="headerbar-text">
+                    {{ isSmallScreen ? ">" : "Cart" }}
+                </div>
                 <object v-html="cartIcon" v-once />
             </button>
         </div>
@@ -23,6 +31,7 @@ import cartIcon from "@/assets/cart.svg?raw";
 import categoryIcon from "@/assets/category.svg?raw";
 import { computed } from "vue";
 import { useRouterNavigation } from "@/composables/useRouterNavigation";
+import { useBreakpoints } from "@vueuse/core";
 
 // Router navigation
 const { navigateToCart, navigateToProducts, router } = useRouterNavigation();
@@ -32,20 +41,34 @@ const currentRoute = computed(() => {
     console.log(router.currentRoute.value.name);
     return router.currentRoute.value.name;
 });
+
+// Breakpoints
+const breakpoints = useBreakpoints({
+    sm: 640, // Tailwind's sm breakpoint
+});
+
+// Check if the screen size is sm or smaller
+const isSmallScreen = breakpoints.smaller("sm");
+
+// Define whether the left button should be shown
+const showLeftButton = computed(() => {
+    return currentRoute.value !== "products" && !isSmallScreen.value;
+});
 </script>
 
 <style scoped>
 .headerbar {
-    @apply text-white w-full h-full p-2 border-b-2 border-gray-700;
+    @apply text-white w-full h-full p-2;
 }
 
 .headerbar-content {
-    @apply w-full h-full flex justify-between items-center p-4;
+    @apply w-full h-full flex justify-end items-center p-4 gap-8;
+    @apply sm:justify-between;
 }
 
-.headerbar-left,
-.headerbar-right {
-    @apply flex items-center justify-around gap-2;
+.headerbar-btn-left,
+.headerbar-btn-right {
+    @apply flex items-center justify-around gap-2 p-1 shadow-gray-900 rounded-md shadow-lg;
 }
 
 .headerbar-center {
