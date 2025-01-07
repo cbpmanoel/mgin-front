716bc2b412dad2cd7827b64a0786ebdd206e5802
Manoel Pereira <cbp.manoel@gmail.com>
2025-01-06 21:10:24 -0300

    Update Dockerfile to use Alpine image and modify Vite config to load environment variables

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 # Use the official Node.js image as the base image
-FROM node:18 AS build-stage
+FROM node:18-alpine AS build-stage
 
 # Set the working directory inside the container
 WORKDIR /app
--- vite.config.js
+++ vite.config.js
@@ -1,27 +1,29 @@
 import { fileURLToPath, URL } from "node:url";
-
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueDevTools from "vite-plugin-vue-devtools";
 import svgLoader from "vite-svg-loader";
 
 // https://vite.dev/config/
-export default defineConfig({
-    plugins: [vue(), vueDevTools(), svgLoader()],
-    resolve: {
-        alias: {
-            "@": fileURLToPath(new URL("./src", import.meta.url)),
-        },
-    },
-    server: {
-        proxy: {
-            "/api": {
-                target:
-                    import.meta.env.VITE_API_BASE_URL ||
-                    "http://localhost:8000",
-                changeOrigin: true,
-                rewrite: (path) => path.replace(/^\/api/, ""),
+export default defineConfig(({ mode }) => {
+    // Load environment variables based on the mode (development, production, etc.)
+    const env = loadEnv(mode, process.cwd(), "VITE_");
+
+    return {
+        plugins: [vue(), vueDevTools(), svgLoader()],
+        resolve: {
+            alias: {
+                "@": fileURLToPath(new URL("./src", import.meta.url)),
             },
         },
-    },
+        server: {
+            proxy: {
+                "/api": {
+                    target: env.VITE_API_BASE_URL || "http://localhost:8000",
+                    changeOrigin: true,
+                    rewrite: (path) => path.replace(/^\/api/, ""),
+                },
+            },
+        },
+    };
 });
